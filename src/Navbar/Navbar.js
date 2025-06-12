8e981a6e1cd13fbc2054c2ad975f216be688f4db
Your Name <your-email@example.com>
2025-06-12 20:42:35 +0500

    adding media

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useState } from 'react'
 import { useStoreState } from "../Redux/selector";
 import locale from "../localization/locale.json";
 import { useDispatch } from "react-redux";
@@ -9,12 +9,16 @@ import { NavLink, useNavigate } from 'react-router-dom'
 import { signOut } from "firebase/auth";
 import { auth } from "../Components/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+
 
 function Navbar() {
     const states = useStoreState();
     const langData = useMemo(() => locale[states.lang], [states.lang]);
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const [showMenu, setShowMenu] = useState(false);
     const [user] = useAuthState(auth); // foydalanuvchi holati
 
     const handleLogout = () => {
@@ -27,40 +31,81 @@ function Navbar() {
     };
 
     return (
-        <div className='Navbar'>
-            <NavLink to='/'><img src={logo} alt="logo" /></NavLink>
+        <>
+            <div className='Navbar'>
+                <NavLink to='/'><img src={logo} alt="logo" /></NavLink>
 
-            <div className="texts">
-                <NavLink to='/Support'><span>{langData.yordam}</span></NavLink>
-                <NavLink to='/ServicesPage'><span>{langData.services}</span></NavLink>
+                <div className="texts">
+                    <NavLink to='/Support'><span>{langData.yordam}</span></NavLink>
+                    <NavLink to='/ServicesPage'><span>{langData.services}</span></NavLink>
+                </div>
+
+                <div className="buttons">
+                    <select
+                        className='language'
+                        value={states.lang}
+                        onChange={(e) => dispatch(setLang(e.target.value))}
+                    >
+                        <option value="uz">UZ</option>
+                        <option value="en">EN</option>
+                        <option value="ru">RU</option>
+                    </select>
+
+                    {user ? (
+                        <>
+                            <NavLink to="/ProfilePage">
+                                <button>{langData.profil}</button>
+                            </NavLink>
+                            <button onClick={handleLogout}>{langData.chiqish}</button>
+                        </>
+                    ) : (
+                        <>
+                            <button><NavLink to="/AdminSignupPage">{langData.register}</NavLink></button>
+                            <button><NavLink to="/AdminLoginPage">{langData.login}</NavLink></button>
+                        </>
+                    )}
+                </div>
             </div>
+            <div className="navbar-media">
+                <NavLink to='/'> <img src={logo} alt="..." className='logo' /></NavLink>
+                <div className="search-panel-media">
+                    <select
+                        className='language-media'
+                        value={states.lang}
+                        onChange={(e) => dispatch(setLang(e.target.value))}
+                    >
+                        <option value="uz">UZ</option>
+                        <option value="en">EN</option>
+                        <option value="ru">RU</option>
 
-            <div className="buttons">
-                <select
-                    className='language'
-                    value={states.lang}
-                    onChange={(e) => dispatch(setLang(e.target.value))}
-                >
-                    <option value="uz">UZ</option>
-                    <option value="en">EN</option>
-                    <option value="ru">RU</option>
-                </select>
+                    </select>
 
-                {user ? (
-                    <>
-                        <NavLink to="/ProfilePage">
-                            <button>{langData.profil}</button>
-                        </NavLink>
-                        <button onClick={handleLogout}>{langData.chiqish}</button>
-                    </>
-                ) : (
-                    <>
-                        <button><NavLink to="/AdminSignupPage">{langData.register}</NavLink></button>
-                        <button><NavLink to="/AdminLoginPage">{langData.login}</NavLink></button>
-                    </>
-                )}
+                    <button className="menu-icon" onClick={() => setShowMenu(!showMenu)}>
+                        <FontAwesomeIcon icon={showMenu ? faTimes : faBars} className='icon' />
+                    </button>
+                </div>
+                <div className={`mobile-menu ${showMenu ? 'open' : ''}`}>
+                    <div className="media-texts">
+                        <NavLink to='/Support'><span>{langData.yordam}</span></NavLink>
+                        <NavLink to='/ServicesPage'><span>{langData.services}</span></NavLink>
+                    </div>
+
+                    {user ? (
+                        <>
+                            <NavLink to="/ProfilePage">
+                                <button>{langData.profil}</button>
+                            </NavLink>
+                            <button onClick={handleLogout}>{langData.chiqish}</button>
+                        </>
+                    ) : (
+                        <>
+                            <button><NavLink to="/AdminSignupPage">{langData.register}</NavLink></button>
+                            <button><NavLink to="/AdminLoginPage">{langData.login}</NavLink></button>
+                        </>
+                    )}
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
